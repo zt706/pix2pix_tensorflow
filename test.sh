324a907c5f71bab762887e4f3c53400727855b9f
canglang <564677762@qq.com>
2018-05-08 15:31:02 +0800

    init

---
+++ test.sh
@@ -0,0 +1,5 @@
+python pix2pix.py \
+		--mode test \
+		--output_dir ./Prostate_data2/test_out \
+		--input_dir ./Prostate_data2/val \
+		--checkpoint ./hujiao/Prostate_data2/train_out
---
+++ train.sh
@@ -0,0 +1,3 @@
+
+#python pix2pix.py   --mode train   --output_dir facades_train   --max_epochs 200   --input_dir facades/train --which_direction BtoA --batch_size 1
+python pix2pix.py   --mode train   --output_dir ./Prostate_data2/train_out   --max_epochs 200   --input_dir ./Prostate_data2/train --which_direction AtoB --batch_size 1
