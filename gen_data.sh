95f191427d4dccc2bb333b93c494b4670ef9f0cf
沧浪 <564677762@qq.com>
2018-05-08 15:35:31 +0800

    Update gen_data.sh

--- gen_data.sh
+++ gen_data.sh
@@ -1,4 +1,4 @@
  
-python tools/process.py --input_dir /world/data-c5/zhouji_demo_3_15/segmention_dir/hujiao/Prostate_data2/Train  --b_dir /world/data-c5/zhouji_demo_3_15/segmention_dir/hujiao/Prostate_data2/Train_Truth --operation combine --output_dir /world/data-c5/zhouji_demo_3_15/segmention_dir/hujiao/Prostate_data2/train
+python tools/process.py --input_dir ./Prostate_data2/Train  --b_dir ./Prostate_data2/Train_Truth --operation combine --output_dir ./Prostate_data2/train
 
-python tools/process.py --input_dir /world/data-c5/zhouji_demo_3_15/segmention_dir/hujiao/Prostate_data2/Val  --b_dir /world/data-c5/zhouji_demo_3_15/segmention_dir/hujiao/Prostate_data2/Val_Truth --operation combine --output_dir /world/data-c5/zhouji_demo_3_15/segmention_dir/hujiao/Prostate_data2/val
+python tools/process.py --input_dir ./Prostate_data2/Val  --b_dir ./Prostate_data2/Val_Truth --operation combine --output_dir ./Prostate_data2/val
